dd13720ab5fcd10557cb80cfa7032307aaa168e2
Josh Buchanan <48423110+jbuchanan00@users.noreply.github.com>
2024-04-29 21:57:15 -0600

    redid some file organization

--- server/app.js
+++ server/app.js
@@ -1,18 +1,7 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
-// Serve static files from the 'build' directory
-app.use(express.static(path.join(__dirname, '../build')));
+app.listen(PORT, () => console.log(`Server started on ${PORT}`));
 
-// Define any additional API routes or middleware here
-
-// For all other requests, serve the React app
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build', 'index.js'));
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+app.use(express.static("../build"));
