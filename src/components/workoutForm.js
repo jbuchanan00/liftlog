92dcd0328143039fa444e0a468a59db8275581b9
Josh Buchanan <48423110+jbuchanan00@users.noreply.github.com>
2024-05-15 21:57:46 -0600

    got select element to work with selecting exercise. Need the id to be the value though passed to the server

--- src/components/workoutForm.js
+++ src/components/workoutForm.js
@@ -2,22 +2,32 @@ import React, {useEffect, useState, useRef} from 'react';
 import axios from 'axios';
 
 const WorkoutForm = () => {
-    const exerciseInput = useRef(null);
+    const [exercises, setExercises] = useState([]);
     const [numOfFields, setNumOfFields] = useState(0);
     const [repFields, setRepFields] = useState([]);
+    const [repWeights, setRepWeights] = useState([]);
+    const [selectedExercise, setSelectedExercise] = useState('');
+    
 
     const renderFields = (e) => {
         const fields = [];
         for(let i = 0; i < numOfFields; i++){
             fields.push(
                 <div>
-                    <label htmlFor={`rep${i}`} style={{display:'hidden'}}></label> 
+                    <label htmlFor={`rep${i}`}>{i} Reps: </label>
                     <input
                         key={i}
                         id={`rep${i}`}
                         name={`rep${i}`}
                         type="number"
                         onChange={(e) => handleRepChange(i, e)}/>
+                    <label htmlFor={`rep${i}weight`}>{i} Rep Weight: </label>
+                    <input
+                        key={i}
+                        id={`rep${i}weight`}
+                        name={`rep${i}weight`}
+                        type="number"
+                        onChange={(e) => handleWeightChange(i, e)}/>
                 </div>
             );
         }
@@ -29,18 +39,31 @@ const WorkoutForm = () => {
     }
 
     const handleRepChange = (i, e) => {
-        console.log(e)
         const repsCopy = [...repFields];
         repsCopy[i] = e.target.value;
         setRepFields(repsCopy);
     }
 
+    const handleWeightChange = (i, e) => {
+        console.log(e)
+        const repsCopy = [...repWeights];
+        repsCopy[i] = e.target.value;
+        setRepWeights(repsCopy);
+    }
+
+    const handleExerciseChange = (e) => {
+        console.log(`selected exercise change ${(e.target.value)}`)
+        setSelectedExercise(e.target.value);
+    }
+
     const handleSubmit = (e) => {
         e.preventDefault();
+        console.log(`Selected exercise ${selectedExercise}`)
         const formData = {
-            exercise: exerciseInput.current.value,
+            exercise: selectedExercise,
             sets: numOfFields,
-            reps: repFields
+            reps: repFields,
+            weights: repWeights
         }
         
         console.log("Submitted: ", {formData})
@@ -56,20 +79,27 @@ const WorkoutForm = () => {
     useEffect(() => {
         axios.get('/exercises')
             .then(response => {
+                setExercises(['bicep curls', 'rows'])
+                //setExercises(response.data)
                 console.log(`this is the response ${JSON.stringify(response.data)}`)
             })
             .catch(error => {
                 console.error(`There was an error ${error}`)
             })
-    })
+            
+    }, [])
 
     return (
         <form onSubmit={handleSubmit}>
             <div>
                 <label htmlFor='workoutName'>Workout Name: </label>
-                <input type='text' id='workoutName' 
-                    name='workoutName' 
-                    ref={exerciseInput}/>
+                <select value={selectedExercise} onChange={handleExerciseChange} name='workoutName'>
+                    {exercises.map((option, index) => {
+                        console.log(option);
+                        //return <option key={index} value={option.workout_name}>{option.workout_name}</option>
+                        return <option key={index} value={option}>{option}</option>
+                    })}
+                </select>
             </div>
             <div>
                 <label htmlFor='numSets'>Number of Sets: </label>
@@ -80,7 +110,7 @@ const WorkoutForm = () => {
                     onChange={handleSetChange}/>
             </div>
             {renderFields()}
-            
+            <p>{JSON.stringify(exercises)}</p>
             <button type='submit'>Submit</button>
         </form>
     )
